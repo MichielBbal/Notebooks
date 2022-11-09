a35f1ab6f9902e0952058500659e1e25f29a2187
Michiel Bontenbal <37507786+MichielBbal@users.noreply.github.com>
2022-11-09 14:53:21 +0100

    10/11
    update 10/11

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 #Mastodon
 mastodon_account = 'michiel.bontenbal@gmail.com'
 mastodon_pw = 'RpDMyvXNJyuhx6t'
+mastodon_client_key = 'oezli5inp_mISOVnBYdVye4nI45nfdZp7xK6xyXosdk'
+mastodon_client_secret = 'Egc5O7-heIusb7deRrbZPzGPtv8lZ-DLvhxWuKRAw7I' 
+mastodon_access_token = 'rxbWelcSEc4F9Mhn2B2mTTwhHIXHGekBObdAygyGX-s'
 
 #Twitter
 
